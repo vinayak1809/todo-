93a359a843d452fbdf8342248115fac80bd1c26b
vinayak1809 <vinayakrshinde18@gmail.com>
2021-11-11 11:09:59 +0530

    code

---
+++ todo.py
@@ -0,0 +1,53 @@
+
+done = False
+
+
+class create_task:
+    li = []
+    di = {}
+    remove_task = {}
+    done2 = False
+
+    def do():
+
+        print("\n if done with entering task : type \"exit\"")
+        while not create_task.done2:
+            en = input(" enter the task : ").lower()
+
+            if en == "exit":
+                create_task.done2 = True
+            elif en:
+                create_task.li.append(en)
+
+            for i, m in enumerate(create_task.li, 1):
+                create_task.di.update({i: m})
+
+            for key, val in create_task.di.items():
+                print(str(key) + ") " + val)
+            print("")
+#            print(create_task.di)
+
+
+class task_done:
+
+    def done():
+        en = int(input("\n enter the integer of the task which is done : "))
+
+        create_task.di.pop(en)
+
+        for key, val in create_task.di.items():
+            print(str(key) + ") " + val)
+
+
+while not done:
+
+    en = input(
+        "\n if you want to enter the task : type \"task\" \n if you remove/done with the task : type \"done\" : ").lower()
+
+    if en == "task":
+        obj = create_task
+        obj.do()
+
+    elif en == "done":
+        obj = task_done
+        obj.done()
